f649912be7f67f3246b5ee7f5bc93a15927d996e
U-DESKTOP-6U572V5\iriss <iyjuanico@ucdavis.edu>
2022-03-02 21:54:29 -0800

    ver1

---
+++ 61dust.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+# 61dust.py
+
+import argparse
+import math
+from mcb185 import *
+# Write a program that finds and masks low entropy sequence
+# Use argparse for the following parameters
+#   sequence file (FASTA)
+#   window size
+#   entropy threshold- calc with Shannon Entropy Calculator
+#   lowercase or N-based masking
+# The program should output a FASTA file (but with Ns or lowercase)
+# Use argparse
+# Use the mcb185.read_fasta() function
+# Put more functions in your mcb185.py library
+
+
+parser = argparse.ArgumentParser(description='Scan FASTA for N-based masking based on entropy threshold.')
+
+parser.add_argument('--fasta', required=True, type=str,
+	metavar='<str>', help='required fasta file')
+parser.add_argument('--winsize', required=True, type=int,
+	metavar='<int>', help='required integer argument')
+parser.add_argument('--ethreshold', required=False, type=float, default=0.70,
+	metavar='<float>', help='optional floating point argument [%(default).3f]')
+parser.add_argument('--linelength', required=False, type=int, default=50,
+	metavar='<int>', help='optional integer argument for fasta line length [%(default)i]')
+parser.add_argument('--lowercase', action='store_true',
+	help='change to lowercase masking')
+arg = parser.parse_args()
+
+#switch for lowercase or N mask
+
+if arg.lowercase: 
+	for name, seq in read_fasta(arg.fasta):
+		rewrite = l_entropy_seq_rewriter(seq, arg.ethreshold, arg.winsize)
+		print(f'>{name}')
+		for i in range(0, len(rewrite), arg.linelength):
+			print(rewrite[i:i+arg.linelength])
+
+else:
+	for name, seq in read_fasta(arg.fasta):
+		rewrite = n_entropy_seq_rewriter(seq, arg.ethreshold, arg.winsize)
+		print(f'>{name}')
+		for i in range(0, len(rewrite), arg.linelength):
+			print(rewrite[i:i+arg.linelength])
+
+
+#redirect greater than >
---
+++ mcb185.py
@@ -0,0 +1,79 @@
+# mcb185.py
+
+import sys
+import gzip
+import math
+
+def read_fasta(filename):
+	name = None
+	seqs = []
+
+	fp = None
+	if filename == '-':
+		fp = sys.stdin
+	elif filename.endswith('.gz'):
+		fp = gzip.open(filename, 'rt')
+	else:
+		fp = open(filename)
+
+	for line in fp.readlines():
+		line = line.rstrip()
+		if line.startswith('>'):
+			if len(seqs) > 0:
+				seq = ''.join(seqs)
+				yield(name, seq)
+				name = line[1:]
+				seqs = []
+			else:
+				name = line[1:]
+		else:
+			seqs.append(line)
+	yield(name, ''.join(seqs))
+	fp.close()
+
+# def other functions...
+
+def entropy_calc(seq):
+	shannon_list = []
+	
+	a_fraction = seq.count('A')/len(seq)
+	c_fraction = seq.count('C')/len(seq)
+	t_fraction = seq.count('T')/len(seq)
+	g_fraction = seq.count('G')/len(seq)
+	
+	if a_fraction > 0: shannon_list.append(a_fraction)
+	if c_fraction > 0: shannon_list.append(c_fraction)
+	if t_fraction > 0: shannon_list.append(t_fraction)
+	if g_fraction > 0: shannon_list.append(g_fraction)
+	
+	entropy = 0
+	for fraction in shannon_list:
+			entropy += (-fraction*math.log2(fraction))
+	return entropy
+
+def n_entropy_seq_rewriter(seq, threshold, w):
+	rewrite_list=[]
+	rewrite_list[:0]=seq
+
+	for i in range(0, len(seq) - w+1):
+		window = seq[i:i+w]
+		if entropy_calc(window) < threshold:
+			for nt in range(w):
+				rewrite_list[i+nt] = 'N'
+	rewrite = "".join(rewrite_list)
+	return rewrite
+
+def l_entropy_seq_rewriter(seq, threshold, w):
+	rewrite_list=[]
+	rewrite_list[:0]=seq
+	
+	for i in range(0, len(seq) - w+1):
+		window = seq[i:i+w]
+		if entropy_calc(window) < threshold:
+			for nt in range(w):
+				if seq[nt] == 'A': rewrite_list[nt+i] = 'a'
+				if seq[nt] == 'C': rewrite_list[nt+i] = 'c'
+				if seq[nt] == 'T': rewrite_list[nt+i] = 't'
+				if seq[nt] == 'G': rewrite_list[nt+i] = 'g'	
+	rewrite = "".join(rewrite_list)
+	return rewrite
